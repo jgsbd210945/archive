781bbaf0c6254b222803711c7b83b6cf0a69bc2d
jgsbd210945 <99208757+jgsbd210945@users.noreply.github.com>
2022-04-07 15:51:23 -0400

    Add files via upload

---
+++ COSI 10a/Jason Gordon PA1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Jason Gordon
+# PA1
+# 9 September 2020
+
+def cheer(x):
+    print(x,x,x)
+    print()
+    
+def stanza_1():
+    print("WE ARE MIGHTY BRANDEIS\nBLEEDING BLUE & WHITE\nWE CAME TO \"BANG THE GAVEL\"")
+    cheer("FIGHT!")
+    
+def stanza_2():
+    print("THERE ARE NO CURLY BRACES")
+    print("WE \\INDENT \\TO \\THE \\RIGHT")
+    print("WE LEARN TO CODE IN PYTHON")
+    cheer("WRITE!")
+    
+def stanza_3():
+    print("WE TRY TO BE EFFICIENT")
+    print("WE PROGRAM THRU THE NIGHT")
+    print("BUT\tWE\tUSED\tTOO\tMUCH\tMEMORY")
+    cheer("BYTE!")
+    
+def stanza_4():
+    print("BRANDEIS COMPUTER SCIENCE")
+    print("LEADS US TO THE LIGHT")
+    print("OUR PROGRAM IS \"CORRECT\" ")
+    cheer("RIGHT!")
+    
+def stanza_5():
+    print("LOUIS D. BRANDEIS")
+    print("HE WAS TRULY BRIGHT")
+    print("WE NEVER BACK DOWN CAUSE WE")
+    cheer("FIGHT!")
+    
+def main():
+    stanza_1()
+    stanza_2()
+    stanza_3()
+    stanza_4()
+    stanza_5()
+
+main()
+
